e62bfff5024b149c77120469c4da92d482b07f09
Yuichi Tateno <hotchpotch@gmail.com>
2021-04-23 19:49:25 +0900

    update mecab

--- similar_entry/tests/tokonezer_test.py
+++ similar_entry/tests/tokonezer_test.py
@@ -4,3 +4,7 @@ from ..tokenizer import mecab
 def test_mecab():
     assert mecab("こんにちは、かわいい犬ですね。") == ["こんにちは", "かわいい", "犬"]
     assert mecab("猫と犬") == ["猫", "犬"]
+
+
+def test_mecab_numeric():
+    assert mecab("こんにちは、100か千ですね。") == ["こんにちは"]
--- similar_entry/tokenizer.py
+++ similar_entry/tokenizer.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 
-def _createMecabTokenizer(target_fetures=["名詞", "形容詞", "感動詞"]):
+def _createMecabTokenizer(target_features=["名詞", "形容詞", "感動詞"], ignore_numeric=True):
     from fugashi import Tagger
 
     tagger = Tagger("-Owakati")
@@ -9,8 +9,9 @@ def _createMecabTokenizer(target_fetures=["名詞", "形容詞", "感動詞"]):
     def tokenizer(text: str) -> list[str]:
         tokens = []
         for word in tagger(text):
-            if word.feature[0] in target_fetures:
-                tokens.append(word.surface)
+            if word.feature[0] in target_features:
+                if not (ignore_numeric and word.feature[1] == "数詞"):
+                    tokens.append(word.surface)
         return tokens
 
     return tokenizer
